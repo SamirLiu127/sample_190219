26e32e24d688db0105076efdb9fa0b9a1c3ffe67
劉世勛 <gip24000@gmail.com>
2019-02-24 00:25:34 +0800

    end of API method

--- main.py
+++ main.py
@@ -1,25 +1,88 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
 
 
 app = Flask(__name__)
-app.config.update(
-    MONGO_URI="mongodb+srv://cluster0-lcunt.gcp.mongodb.net/rent_house",
-    MONGO_USERNAME="user_01",
-    MONGO_PASSWORD='mCpvS3ThcK9hKfv'
-)
+app.config['MONGO_URI'] = "mongodb+srv://user_reader:8d8iNPusNaPgXfw@cluster0-lcunt.gcp.mongodb.net/rent_house"
 mongo = PyMongo(app)
 
 
 @app.route("/")
 def home_page():
-    response = mongo.db.house591.find({
-        "linkman": "\u5433"
+    response = mongo.db.rent591.find({
+        "linkman": {"$regex": "^林.*"}
     })
-    print(response)
-    return response
+    output = []
+    for doc in response:
+        del doc['_id']
+        output.append(doc)
+    return jsonify(output)
+
+
+@app.route("/search")
+def search():
+    region = request.args.get('region', '')
+    gender = request.args.get('gender', '')
+    owner_name = request.args.get('owner_name', '')
+    owner_gender = request.args.get('owner_gender', '')
+    owner_self = request.args.get('owner_self', '')
+    owner_phone = request.args.get('owner_phone', '')
+
+    query = {}
+    if region:
+        query.update(get_region(region))
+    if gender:
+        query.update(get_gender(gender))
+    if owner_name or owner_gender:
+        query.update(get_owner(owner_name, owner_gender))
+    if owner_self != '':
+        query.update(get_owner_self(owner_self))
+    if owner_phone:
+        query.update(get_owner_phone(owner_phone))
+
+    print(query)
+    response = mongo.db.rent591.find(query)
+    output = []
+    for doc in response:
+        del doc['_id']
+        output.append(doc)
+    return jsonify(output)
+
+
+def get_region(num):
+    data = {
+        '1': '台北市',
+        '3': '新北市'
+    }
+    return {"region_name": data.get(num, "台北市")}
+
+def get_gender(gender):
+    data = {
+        '0': ["女生", "", "男女生皆可"],
+        '1': ["男生", "", "男女生皆可"]
+    }
+    return {"gender": {"$in": data[gender]}}
+
+def get_owner(owner_name, owner_gender):
+    gender_data = {
+        '0': "小姐",
+        '1': "先生"
+    }
+    name = "^" + owner_name if owner_name else ".*"
+    gender = gender_data[owner_gender] + "$" if owner_gender else ".*"
+    return {"linkman": {"$regex": name + gender}}
+
+def get_owner_self(owner_self):
+    data = {
+        '0': "^((?!屋主).)*$",
+        '1': "^屋主.*"
+    }
+    return {"nick_name": {"$regex": data[owner_self]}}
+
+def get_owner_phone(phone):
+    return {"phone_number": {"$regex": f"^{phone}.*"}}
 
 
 if __name__ == '__main__':
